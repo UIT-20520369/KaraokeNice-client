17373cfe53bf3c34930d9df26fe332acfce2eba7
Vương Trương Xuân <20520353@ms.uit.edu.vn>
2023-01-11 13:11:04 +0700

    Test ReactJS

---
+++ karaoke-client/src/features/Todo/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import TodoList from "./components/TodoList";
+
+TodoFeature.propTypes = {
+
+};
+
+function TodoFeature(props) {
+    const todoList = [
+        {
+            id: 1,
+            title: 'Eat',
+        },
+        {
+            id: 2,
+            title: 'Sleep',
+        },
+        {
+            id: 3,
+            title: 'Walk',
+        },
+        {
+            id: 4,
+            title: 'Drink',
+        },
+        {
+            id: 5,
+            title: 'Code',
+        },
+    ]
+
+    return (
+        <div>
+            <h3>Todo List</h3>
+            <TodoList todoList={todoList} />
+        </div>
+    );
+}
+
+export default TodoFeature;
